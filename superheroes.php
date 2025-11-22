151a271f37cd640f21d3774b5555aacf20e1ddac
zaynewestly87 <129535249+zaynewestly87@users.noreply.github.com>
2025-11-22 10:40:11 -0500

    Completed files with AJAX
    CSS, HTML and JavaScript files added and completed.

--- superheroes.php
+++ superheroes.php
@@ -1,72 +1,71 @@
 <?php
 
 $superheroes = [
-  [
-      "id" => 1,
-      "name" => "Steve Rogers",
-      "alias" => "Captain America",
-      "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.",
-  ],
-  [
-      "id" => 2,
-      "name" => "Tony Stark",
-      "alias" => "Ironman",
-      "biography" => "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.",
-  ],
-  [
-      "id" => 3,
-      "name" => "Peter Parker",
-      "alias" => "Spiderman",
-      "biography" => "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
-  ],
-  [
-      "id" => 4,
-      "name" => "Carol Danvers",
-      "alias" => "Captain Marvel",
-      "biography" => "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
-  ],
-  [
-      "id" => 5,
-      "name" => "Natasha Romanov",
-      "alias" => "Black Widow",
-      "biography" => "Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers.",
-  ],
-  [
-      "id" => 6,
-      "name" => "Bruce Banner",
-      "alias" => "Hulk",
-      "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.",
-  ],
-  [
-      "id" => 7,
-      "name" => "Clint Barton",
-      "alias" => "Hawkeye",
-      "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer.",
-  ],
-  [
-      "id" => 8,
-      "name" => "T'challa",
-      "alias" => "Black Panther",
-      "biography" => "T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
-  ],
-  [
-      "id" => 9,
-      "name" => "Thor Odinson",
-      "alias" => "Thor",
-      "biography" => "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike.",
-  ],
-  [
-      "id" => 10,
-      "name" => "Wanda Maximoff",
-      "alias" => "Scarlett Witch",
-      "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
-  ], 
+    [
+        "alias" => "Superman",
+        "name" => "Clark Kent",
+        "biography" => "Superman is a fictional superhero appearing in American comic books published by DC Comics."
+    ],
+    [
+        "alias" => "Batman",
+        "name" => "Bruce Wayne",
+        "biography" => "Batman is a superhero appearing in American comic books published by DC Comics."
+    ],
+    [
+        "alias" => "Wonder Woman",
+        "name" => "Diana Prince",
+        "biography" => "Wonder Woman is a superhero appearing in American comic books published by DC Comics."
+    ],
+    [
+        "alias" => "Flash",
+        "name" => "Barry Allen",
+        "biography" => "The Flash is a superhero appearing in American comic books published by DC Comics."
+    ],
+    [
+        "alias" => "Iron Man",
+        "name" => "Tony Stark",
+        "biography" => "Iron Man is a superhero appearing in American comic books published by Marvel Comics."
+    ],
+    [
+        "alias" => "Captain America",
+        "name" => "Steve Rogers",
+        "biography" => "Captain America is a superhero appearing in American comic books published by Marvel Comics."
+    ],
 ];
 
-?>
+$query = $_GET['query'] ?? '';
+$query = trim($query);
 
-<ul>
-<?php foreach ($superheroes as $superhero): ?>
-  <li><?= $superhero['alias']; ?></li>
-<?php endforeach; ?>
-</ul>
+header("Access-Control-Allow-Origin: *");
+
+if ($query === '') {
+    // ----- Return full list -----
+    echo "<ul>";
+    foreach ($superheroes as $hero) {
+        echo "<li>{$hero['alias']}</li>";
+    }
+    echo "</ul>";
+    exit;
+}
+
+// Sanitize input
+$query = filter_var($query, FILTER_SANITIZE_STRING);
+
+// ----- Search for exact or partial match (case-insensitive) -----
+$found = null;
+
+foreach ($superheroes as $hero) {
+    if (stripos($hero['alias'], $query) !== false || 
+        stripos($hero['name'], $query) !== false) {
+        $found = $hero;
+        break;
+    }
+}
+
+if ($found) {
+    echo "<h3>{$found['alias']}</h3>";
+    echo "<h4>{$found['name']}</h4>";
+    echo "<p>{$found['biography']}</p>";
+} else {
+    echo "<p>Superhero not found</p>";
+}
